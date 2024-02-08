fedea0845d7110257a118610e1a7e43539902cd4
JoiAkito <amoe16567@gmail.com>
2024-02-08 23:47:01 +0630

    using auth sanctum features

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->longText('access_token')->nullable();
             $table->longText('refresh_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
